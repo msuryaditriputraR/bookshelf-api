0a7773331cc440e94ac882dc662c3180462cd7b7
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-05-29 22:47:21 +0800

    fix bugs response if name == dicoding

--- src/handler/getAllBooksHandler.js
+++ src/handler/getAllBooksHandler.js
@@ -13,46 +13,48 @@ const getAllBooksHandler = (request, h) => {
 
     const { name, reading, finished } = request.query;
 
-    if (name.toLowerCase() === 'dicoding') {
-        const response = h.response({
-            status: 'success',
-            data: {
-                books: books.map(book => {
-                    if (book.name.toLowerCase().includes('dicoding')) {
-                        return book;
-                    }
-                })
-            }
-        });
-        response.code(200);
-        return response;
+    if (name) {
+        if (name.toLowerCase() === 'dicoding') {
+            const response = h.response({
+                status: 'success',
+                data: {
+                    books: newBooks.filter(book => {
+                        if (book.name.toLowerCase().includes('dicoding')) {
+                            return book;
+                        }
+                    })
+                }
+            });
+            response.code(200);
+            return response;
+        }
     }
 
-    const booksReading = books.filter(book => book.reading == reading);
+    // const booksReading = books.filter(book => book.reading == reading);
 
-    if (booksReading.length > 0) {
-        const response = h.response({
-            status: 'success',
-            data: {
-                books: booksReading
-            }
-        });
-        response.code(200);
-        return response;
-    }
+    // if (booksReading.length > 0) {
+    //     const response = h.response({
+    //         status: 'success',
+    //         data: {
+    //             books: booksReading
+    //         }
+    //     });
+    //     response.code(200);
+    //     return response;
+    // }
 
-    const booksFinished = books.filter(book => book.finished == finished);
+    // const booksFinished = books.filter(book => book.finished == finished);
 
-    if (booksFinished.length > 0) {
-        const response = h.response({
-            status: 'success',
-            data: {
-                books: booksFinished
-            }
-        });
-        response.code(200);
-        return response;
-    }
+    // if (booksFinished.length > 0) {
+    //     const response = h.response({
+    //         status: 'success',
+    //         data: {
+    //             books: booksFinished
+    //         }
+    //     });
+    //     response.code(200);
+    //     return response;
+    // }
 
     const response = h.response({
         status: 'success',
