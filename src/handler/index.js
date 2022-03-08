2459bb508c5150911eb77f7f5e4ec0ed3be6e741
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-03-08 18:23:51 +0800

    refactoring import

--- src/handler/index.js
+++ src/handler/index.js
@@ -1,8 +1,8 @@
-const addBookHandler = require('./handler/addBookHandler');
-const getAllBooksHandler = require('./handler/getAllBooksHandler');
-const getDetailBookHandler = require('./handler/getDetailBookHandler');
-const editBookHandler = require('./handler/editBookHandler');
-const deleteBookHandler = require('./handler/deleteBookHandler');
+const addBookHandler = require('./addBookHandler');
+const getAllBooksHandler = require('./getAllBooksHandler');
+const getDetailBookHandler = require('./getDetailBookHandler');
+const editBookHandler = require('./editBookHandler');
+const deleteBookHandler = require('./deleteBookHandler');
 
 module.exports = {
     addBookHandler,
